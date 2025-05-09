b5957b427b23a634e432271ebfe9765f07f68237
Ilya Razmanov <ilyarazmanov@gmail.com>
2025-05-09 10:57:23 +0300

    9 May 2025 "Victory" release. Autofocus fixes, Python 3.10 compatibility.

--- POVRayThread.py
+++ POVRayThread.py
@@ -35,7 +35,7 @@ __author__ = 'Ilya Razmanov'
 __copyright__ = '(c) 2024-2025 Ilya Razmanov'
 __credits__ = 'Ilya Razmanov'
 __license__ = 'unlicense'
-__version__ = '1.16.20.20'
+__version__ = '1.17.9.11'
 __maintainer__ = 'Ilya Razmanov'
 __email__ = 'ilyarazmanov@gmail.com'
 __status__ = 'Production'
@@ -111,6 +111,7 @@ def GetSource(event=None) -> None:
     global preview
     global X, Y, Z, maxcolors, image3D, info, sourcefilename
     global source_image3D, preview_src, preview_filtered  # deep copy of source data and copies of preview
+    global info_normal
 
     zoom_factor = 0
     view_src = True
@@ -120,6 +121,8 @@ def GetSource(event=None) -> None:
     sourcefilename = filedialog.askopenfilename(title='Open image file', filetypes=[('Supported formats', '.png .ppm .pgm .pbm'), ('PNG', '.png'), ('PNM', '.ppm .pgm .pbm')])
     if sourcefilename == '':
         return
+
+    info_normal = {'txt': f'{Path(sourcefilename).name}', 'fg': 'grey', 'bg': 'grey90'}
 
     UIBusy()
 
